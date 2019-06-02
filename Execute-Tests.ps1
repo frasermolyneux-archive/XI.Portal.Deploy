97d97c2c7085f8885e8b74ae719dbb31a07641a5
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2019-06-02 16:38:31 +0100

    Removing functions load

--- Execute-Tests.ps1
+++ Execute-Tests.ps1
@@ -7,10 +7,6 @@ Set-Location $PSScriptRoot
 
 $moduleRoot = "$PSScriptRoot"
 
-Get-ChildItem -Path "$PSScriptRoot\Functions" -Filter "*.ps1" | ForEach-Object {
-    . $_.FullName
-}
-
 Get-ChildItem -Path "$PSScriptRoot\Tests" -Filter "*.Tests.ps1" | ForEach-Object {
     . $_.FullName -ModuleRoot $moduleRoot
 }
